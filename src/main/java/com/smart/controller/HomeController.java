0f3f72dbddce8081b4b9be67ef36644d302cde62
Rahul Suthar <rahulsuthar@RAHULs-MacBook.local>
2022-09-27 15:25:23 +0530

    first commit

---
+++ src/main/java/com/smart/controller/HomeController.java
@@ -0,0 +1,82 @@
+package com.smart.controller;
+
+import com.smart.dao.UserRepository;
+import com.smart.entities.User;
+import com.smart.helper.Message;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
+
+@Controller
+public class HomeController {
+
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @RequestMapping("/")
+    public String home(Model model) {
+        model.addAttribute("title", "Home - Smart Contact Manager");
+        return "home";
+    }
+
+    @RequestMapping("/about")
+    public String about(Model model) {
+        model.addAttribute("title", "About - Smart Contact Manager");
+        return "about";
+    }
+
+    @RequestMapping("/signup")
+    public String signup(Model model) {
+        model.addAttribute("title", "Register - Smart Contact Manager");
+        model.addAttribute("user", new User());
+        return "signup";
+    }
+
+    // Handler for registering user
+    @RequestMapping(value = "/do_register", method = RequestMethod.POST)
+    public String registerUser(@ModelAttribute("user") User user,
+                               @RequestParam(value = "agreement", defaultValue = "false") boolean agreement,
+                               Model model, HttpSession session) {
+        try {
+            if (!agreement) {
+                System.out.println("you have not agreed terms and conditions");
+                throw new Exception("you have not agreed terms and conditions");
+            }
+            user.setRole("ROLE_USER");
+            user.setEnabled(true);
+            user.setImageUrl("default.png");
+            user.setPassword(passwordEncoder.encode(user.getPassword()));
+
+            System.out.println("Agreement " + agreement);
+            System.out.println("User " + user);
+
+            User result = this.userRepository.save(user);
+            model.addAttribute("user", new User());
+
+            session.setAttribute("message", new Message("Successfully Registered !!", "alert-success"));
+            return "signup";
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            model.addAttribute("user", user);
+            session.setAttribute("message", new Message("Something went wrong " + e.getMessage(), "alert-danger"));
+            return "signup";
+        }
+    }
+
+    // Handler for login
+    @GetMapping("/signin")
+    public String login(Model model){
+        return "login";
+    }
+}
+
