0f0f6eaa2da75d6166da1ac13ced11b2a70c44e1
Rahul Suthar <rahulsuthar@RAHULs-MacBook.local>
2022-09-28 23:31:22 +0530

    created contact list

--- src/main/java/com/smart/controller/UserController.java
+++ src/main/java/com/smart/controller/UserController.java
@@ -1,8 +1,10 @@
 package com.smart.controller;
 
+import com.smart.dao.ContactRepository;
 import com.smart.dao.UserRepository;
 import com.smart.entities.Contact;
 import com.smart.entities.User;
+import com.smart.helper.Message;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Controller;
@@ -10,12 +12,14 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpSession;
 import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 import java.security.Principal;
+import java.util.List;
 
 @Controller
 @RequestMapping("/user")
@@ -23,6 +27,9 @@ public class UserController {
 
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private ContactRepository contactRepository;
 
     //    Method for adding common to response
     @ModelAttribute
@@ -55,37 +62,43 @@ public class UserController {
     //    processing  add contact form
     @PostMapping("/process-contact")
     public String processContact(@ModelAttribute Contact contact,
-                                 @RequestParam("profileImage") MultipartFile file, Principal principal) {
+                                 @RequestParam("profileImage") MultipartFile file, Principal principal, HttpSession session) {
         try {
-
             String name = principal.getName();
             User user = userRepository.getUserByUserName(name);
-
 //         processing and uploading file
             if (file.isEmpty()) {
                 System.out.println("File is empty");
             } else {
                 contact.setImage(file.getOriginalFilename());
-
                 File saveFile = new ClassPathResource("static/img").getFile();
-
                 Path path = Paths.get(saveFile.getAbsolutePath() + File.separator + file.getOriginalFilename());
-
                 Files.copy(file.getInputStream(), path, StandardCopyOption.REPLACE_EXISTING);
-
                 System.out.println("Image is uploaded");
             }
-
             contact.setUser(user);
             user.getContacts().add(contact);
-
             this.userRepository.save(user);
+            session.setAttribute("message", new Message("Your Contact is added !! Add more..", "success"));
         } catch (Exception e) {
             System.out.println("ERROR");
             e.printStackTrace();
+            session.setAttribute("message", new Message("Something went wrong !! Try again", "danger"));
         }
         return "normal/add_contact_form";
     }
 
+    // show contact handler
+    @GetMapping("/show-contacts")
+    public String showContacts(Model model,Principal principal){
+        model.addAttribute("title","Show User Contacts");
+
+        String userName = principal.getName();
+        User user = this.userRepository.getUserByUserName(userName);
+        List<Contact> contacts = this.contactRepository.findContactByUser(user.getId());
+        model.addAttribute("contacts",contacts);
+
+        return "normal/show_contacts";
+    }
 
 }
