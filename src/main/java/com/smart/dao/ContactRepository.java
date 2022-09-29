208b3d669deb1ab88008bfdc5d8b7109eb51540f
Rahul Suthar <rahulsuthar@RAHULs-MacBook.local>
2022-09-29 14:06:52 +0530

    added @Parm in ContactRepo

--- src/main/java/com/smart/dao/ContactRepository.java
+++ src/main/java/com/smart/dao/ContactRepository.java
@@ -1,13 +1,14 @@
 package com.smart.dao;
 
 import com.smart.entities.Contact;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import java.util.List;
-
 public interface ContactRepository extends JpaRepository<Contact,Integer> {
+//    pageable takes => contactPage -page and contact per page
     @Query("from Contact as c where c.user.id=:userId")
-    public List<Contact> findContactByUser(@Param("userId")int userId);
+    public Page<Contact> findContactByUser(@Param("userId")int userId, Pageable pageable);
 }
