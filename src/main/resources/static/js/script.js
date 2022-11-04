5cbb21f35c39acbd2d8af95fc798ac950ea19203
Rahul Suthar <rahulsuthar@RAHULs-MacBook.local>
2022-11-04 23:27:45 +0530

    home page background changed

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -10,26 +10,26 @@ const toggleSidebar = () => {
     }
 };
 // for searching
-const search=()=>{
+const search = () => {
     const query = $("#search-input").val();
 
-    if(query == ''){
-    $(".search-result").hide();
-    }else {
-    //    searching
-    //sending request to server
+    if (query == '') {
+        $(".search-result").hide();
+    } else {
+        //    searching
+        //sending request to server
         let url = `http://localhost:8080/search/${query}`;
 
         fetch(url).then((response) => {
             return response.json();
-        }).then((data) =>{
-            let text =`<div class='list-group'>`;
-            data.forEach((contact)=>{
-                text+=`<a href='/user/${contact.cId}/contact' class='list-group-item list-group-action'>${contact.name}</a>`;
+        }).then((data) => {
+            let text = `<div class='list-group'>`;
+            data.forEach((contact) => {
+                text += `<a href='/user/${contact.cId}/contact' class='list-group-item list-group-action'>${contact.name}</a>`;
             });
-            text+=`</div>`;
-           $(".search-result").html(text);
-           $(".search-result").show();
+            text += `</div>`;
+            $(".search-result").html(text);
+            $(".search-result").show();
         })
     }
 }
